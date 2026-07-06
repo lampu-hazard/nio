5fe889f0eca7cb75012da8216c3211a4c0923c87
wign <tigfiragnafatur1933@gmail.com>
2026-07-06 14:18:40 +0700

    feat(guilds): update imports to use forwardRef for DiscordModule

--- apps/server/src/guilds/guilds.module.ts
+++ apps/server/src/guilds/guilds.module.ts
@@ -1,11 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { DiscordModule } from '../discord/discord.module';
 import { StickersModule } from '../stickers/stickers.module';
 import { GuildsController } from './guilds.controller';
 import { GuildsService } from './guilds.service';
 
 @Module({
-  imports: [DiscordModule, StickersModule],
+  imports: [forwardRef(() => DiscordModule), StickersModule],
   controllers: [GuildsController],
   providers: [GuildsService],
   exports: [GuildsService],
